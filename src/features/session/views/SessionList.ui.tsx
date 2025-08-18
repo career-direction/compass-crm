d45901f3f295c3afb3a0ce1e5d389b2dee96373a
uabyss <liberalist0449@gmail.com>
2025-08-18 20:52:27 +0900

    add: Title

--- src/features/session/views/SessionList.ui.tsx
+++ src/features/session/views/SessionList.ui.tsx
@@ -1,17 +1,22 @@
 "use client";
 
-import { List } from "@mantine/core";
+import { Box, List, Title } from "@mantine/core";
 import { SessionListItem } from "../components/SessionListItem.ui";
 
 export const SessionList = () => {
 	return (
-		<List>
-			<List.Item>
-				<SessionListItem />
-			</List.Item>
-			<List.Item>
-				<SessionListItem />
-			</List.Item>
-		</List>
+		<>
+			<Box mb="md">
+				<Title>セッション一覧</Title>
+			</Box>
+			<List>
+				<List.Item>
+					<SessionListItem />
+				</List.Item>
+				<List.Item>
+					<SessionListItem />
+				</List.Item>
+			</List>
+		</>
 	);
 };
