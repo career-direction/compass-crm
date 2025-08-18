5a44d1ab7103521c598bc692dd748bb4fb0b17ff
uabyss <liberalist0449@gmail.com>
2025-08-18 21:03:26 +0900

    add: filter item

--- src/app/session/page.tsx
+++ src/app/session/page.tsx
@@ -3,7 +3,7 @@ import { SessionList } from "@/features/session/views/SessionList.ui";
 
 export default function RegisterPage() {
 	return (
-		<Container size={420} my={40}>
+		<Container>
 			<SessionList />
 		</Container>
 	);
--- src/features/session/views/SessionList.ui.tsx
+++ src/features/session/views/SessionList.ui.tsx
@@ -1,14 +1,45 @@
 "use client";
 
-import { Box, List, Title } from "@mantine/core";
+import {
+	Box,
+	Button,
+	Group,
+	List,
+	Space,
+	Title,
+	Text,
+	SimpleGrid,
+	Chip,
+} from "@mantine/core";
 import { SessionListItem } from "../components/SessionListItem.ui";
 
 export const SessionList = () => {
 	return (
 		<>
-			<Box mb="md">
-				<Title>セッション一覧</Title>
-			</Box>
+			<Group justify="space-between" align="center" my="md">
+				<Box>
+					<Title>セッション一覧</Title>
+				</Box>
+				<Space></Space>
+				<Group>
+					<Button>フィルター</Button>
+					<Button>セッション追加</Button>
+				</Group>
+			</Group>
+
+			{/* フィルター条件 */}
+			<Group mb="md" align="start">
+				<Text>フィルター条件:</Text>
+				<SimpleGrid cols={5}>
+					<Chip defaultChecked>Awesome chip</Chip>
+					<Chip defaultChecked>Awesome chip</Chip>
+					<Chip defaultChecked>Awesome chip</Chip>
+					<Chip defaultChecked>Awesome chip</Chip>
+					<Chip defaultChecked>Awesome chip</Chip>
+					<Chip defaultChecked>Awesome chip</Chip>
+				</SimpleGrid>
+			</Group>
+
 			<List>
 				<List.Item>
 					<SessionListItem />
