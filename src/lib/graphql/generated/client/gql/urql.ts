5de51cb2762aa52037e3bd08c7b56b536ac9748c
YuyaOkuse <liberalist0449@gmail.com>
2025-12-30 23:53:08 +0900

    resolve conflicts

--- src/lib/graphql/generated/client/gql/urql.ts
+++ src/lib/graphql/generated/client/gql/urql.ts
@@ -1,19 +1,34 @@
-import * as Operations from './graphql';
-import * as Urql from 'urql';
+import * as Operations from "./graphql";
+import * as Urql from "urql";
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 
-
-<<<<<<< HEAD:src/lib/graphql/generated/client/gql/urql.ts
 export function useCreateLearningMaterialMutation() {
-  return Urql.useMutation<Operations.CreateLearningMaterialMutation, Operations.CreateLearningMaterialMutationVariables>(Operations.CreateLearningMaterialDocument);
-};
+	return Urql.useMutation<
+		Operations.CreateLearningMaterialMutation,
+		Operations.CreateLearningMaterialMutationVariables
+	>(Operations.CreateLearningMaterialDocument);
+}
 
-export function useGetLearningMaterialsQuery(options?: Omit<Urql.UseQueryArgs<Operations.GetLearningMaterialsQueryVariables>, 'query'>) {
-  return Urql.useQuery<Operations.GetLearningMaterialsQuery, Operations.GetLearningMaterialsQueryVariables>({ query: Operations.GetLearningMaterialsDocument, ...options });
-};
+export function useGetLearningMaterialsQuery(
+	options?: Omit<
+		Urql.UseQueryArgs<Operations.GetLearningMaterialsQueryVariables>,
+		"query"
+	>,
+) {
+	return Urql.useQuery<
+		Operations.GetLearningMaterialsQuery,
+		Operations.GetLearningMaterialsQueryVariables
+	>({ query: Operations.GetLearningMaterialsDocument, ...options });
+}
 
-=======
->>>>>>> 9b1a8144a161edc3f732dff517141fba942a4a55:src/graphql/generated/client/gql/urql.ts
-export function useGetTrainersQuery(options?: Omit<Urql.UseQueryArgs<Operations.GetTrainersQueryVariables>, 'query'>) {
-  return Urql.useQuery<Operations.GetTrainersQuery, Operations.GetTrainersQueryVariables>({ query: Operations.GetTrainersDocument, ...options });
-};
+export function useGetTrainersQuery(
+	options?: Omit<
+		Urql.UseQueryArgs<Operations.GetTrainersQueryVariables>,
+		"query"
+	>,
+) {
+	return Urql.useQuery<
+		Operations.GetTrainersQuery,
+		Operations.GetTrainersQueryVariables
+	>({ query: Operations.GetTrainersDocument, ...options });
+}
